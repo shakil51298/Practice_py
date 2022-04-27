27355f6af7018a867eb1daae802f066e7d24f632
mdkhalidhossain <zikaiwen02@gmail.com>
2022-04-27 21:32:19 +0800

    Lists python added

---
+++ Pycharm Projects/Practice Python/Lists.py
@@ -0,0 +1,13 @@
+
+lucky_number = [4, 8, 15, 16, 23, 42]
+friends = ["kevin", "rokon", "shakil", " oscar"]
+friends.extend(lucky_number)
+friends.append("shakil")
+friends.insert(2, "new name")
+friends.remove("new name")
+friends.pop()
+# friends[2] = "Khan"
+# friends.clear();
+print(friends)
+
+
