8c2b92f7fc6c051417b7beebb2c08b4173c3cbbd
mdkhalidhossain <zikaiwen02@gmail.com>
2022-04-27 20:10:27 +0800

    MadLibs game added

---
+++ Pycharm Projects/Practice Python/GettingInputFromUser.py
@@ -0,0 +1,12 @@
+# name = input("Please enter your name: ")
+# age = input("Please enter your age: ")
+#
+# print("hello " + name + "!" + "you are " +  age)
+
+# basic calculator
+
+num1 = input("please enter first number: ")
+num2 = input("plsease enter second number: ")
+
+result = float(num1) + float(num2)
+print(result)
---
+++ Pycharm Projects/Practice Python/MadLibsGame.py
@@ -0,0 +1,9 @@
+
+color = input("enter colorname")
+pluiral_noun = input("enter pluiral noun")
+celebrity = input("enter colorname")
+
+
+print("Roses are " + color)
+print("{pluiral noun} are blue" + pluiral_noun)
+print("I love " + celebrity)
